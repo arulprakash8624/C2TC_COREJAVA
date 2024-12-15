1f587e3fbfde6ca781a2211b58e9d3484f79df8a
arulprakash8624@gmail.com <140597872+arulprakash8624@users.noreply.github.com>
2024-12-15 17:43:48 +0530

    assignment_commit

---
+++ C2TC_ARULPRAKASH/src/com/arulprakash/assignement1/AssignmentMain.java
@@ -0,0 +1,17 @@
+package com.arulprakash.assignement1;
+
+import com.arulprakash.assignment.employees.Developer;
+import com.arulprakash.assignment.employees.Manager;
+import com.arulprakash.assignment.utilities.EmployeeUtilities;
+
+public class AssignmentMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Manager manager = new Manager("John", 101, 90000, "Sales"); 
+	        Developer developer = new Developer("Alice", 102, 80000, "Java"); 
+	        EmployeeUtilities.printEmployeeDetails(manager); 
+	        EmployeeUtilities.printEmployeeDetails(developer);
+	}
+
+}
---
+++ C2TC_ARULPRAKASH/src/com/arulprakash/assignment/utilities/EmployeeUtilities.java
@@ -0,0 +1,21 @@
+package com.arulprakash.assignment.utilities;
+
+import com.arulprakash.assignment.employees.Developer;
+import com.arulprakash.assignment.employees.Employee;
+import com.arulprakash.assignment.employees.Manager;
+
+public class EmployeeUtilities {
+	public static void printEmployeeDetails(Employee employee) { 
+        System.out.println("Employee Name: " + employee.getName()); 
+        System.out.println("Employee ID: " + employee.getEmployeeId()); 
+        System.out.println("Salary: " + employee.getSalary()); 
+        if (employee instanceof Manager) { 
+            Manager manager = (Manager) employee; 
+            System.out.println("Department: " + manager.getDepartment()); 
+        } else if (employee instanceof Developer) { 
+            Developer developer = (Developer) employee; 
+            System.out.println("Programming Language: " + developer.getProgrammingLanguage()); 
+        } 
+        System.out.println(); 
+    }
+}
